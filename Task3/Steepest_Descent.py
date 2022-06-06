dcd214ec90856048eeed0d381355ea21bb42f830
Kenspectacle <Kennyftw2@gmail.com>
2022-06-06 17:18:19 +0200

    fixed Steepest Gradient Function, added rounding

--- Task3/Steepest_Descent.py
+++ Task3/Steepest_Descent.py
@@ -1,3 +1,4 @@
+from cmath import sqrt
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -11,8 +12,16 @@ def Derivatives(x, y):
     f_y = 200 * (y - x ** 2)
     return f_x , f_y
 
-# def Steepest_Gradient_Descent(step_dampener, step_multiplier, f_x, f_y):
-#     return 0
+def Steepest_Gradient_Descent(step_dampener, step_multiplier, f_x, f_y, current_x, current_y):
+    #compute direction step
+    direction_step = [-f_x, -f_y]
+    #compute multiplied step
+    multiplied_step = direction_step * step_multiplier
+    #compute new step multiplier
+    new_step_multiplier = step_multiplier * step_dampener
+    #compute new position
+    new_x, new_y = [current_x + multiplied_step[0], current_y + multiplied_step[1]]
+    return new_x, new_y, new_step_multiplier;
 
 if __name__ == "__main__":
     Rosenbrock_result = []
@@ -35,5 +44,23 @@ if __name__ == "__main__":
     current_y = 0.5
     iteration_count = 0
     step_multiplier = 1.0
-    #while(step_size < 0.5 || iteration_count < 10):
+    step_dampener = 0.5
+    epsilon = 0.01
+    print("hello World")
+
+    f_x, f_y = Derivatives(current_x, current_y)
+    step_size_x = step_multiplier * f_x
+    step_size_y = step_multiplier * f_y
+    step_size_magnitude = sqrt(step_size_x ** 2 + step_size_y ** 2)
+    print(step_size_magnitude)
+    while(step_size_magnitude < epsilon or iteration_count < 10):
+        print("Iteration Number: ", iteration_count)
+        print("Current_x:", current_x)
+        print("Current_y:", current_y)
+        iteration_count += 1
+        f_x, f_y = Derivatives(current_x, current_y)
+        step_size_x = step_multiplier * f_x
+        step_size_y = step_multiplier * f_y
+        step_size_magnitude = sqrt(step_size_x ** 2 + step_size_y ** 2)
+        current_x, current_y, step_multiplier = Steepest_Gradient_Descent(step_dampener, step_multiplier, f_x, f_y, current_x, current_y)
 
